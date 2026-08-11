ef70e0949b53c368c90abae53b8105ab48e295d0
Akshobya A S <asakshobya@gmail.com>
2026-08-12 00:20:05 +0530

    fix: update vercel.json buildCommand and wsgi migrate

--- fstp/wsgi.py
+++ fstp/wsgi.py
@@ -1,5 +1,13 @@
-import os
+﻿import os
 from django.core.wsgi import get_wsgi_application
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fstp.settings')
+
+from django.core.management import call_command
+try:
+    call_command('migrate', '--run-syncdb', verbosity=0)
+except Exception:
+    pass
+
 application = get_wsgi_application()
 app = application
